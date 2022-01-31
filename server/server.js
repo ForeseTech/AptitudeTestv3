2aa1a2a9c519b415c840e60e387685fd23ed1335
NileshMSD <nilesh@madstreetden.com>
2022-01-31 12:23:46 +0530

    Folder

--- server/server.js
+++ server/server.js
@@ -24,10 +24,10 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/timer', timerRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
