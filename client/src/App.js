57261b66a957d2d601a908a9b350a047e6e67cff
NileshMSD <nilesh@madstreetden.com>
2022-02-01 10:56:39 +0530

    Don't show header on Test Screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import Header from './components/Header';
 import LoginScreen from './screens/LoginScreen';
@@ -14,7 +14,7 @@ import AdminEditQuestionScreen from './screens/AdminEditQuestionScreen';
 
 const theme = createTheme({
   typography: {
-    fontFamily: ['Poppins', 'sans-serif'].join(','),
+    fontFamily: ['Andada Pro', 'serif'].join(','),
   },
 });
 
@@ -22,17 +22,26 @@ function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <Header />
         <main className='app'>
-          <Route path='/' component={LoginScreen} exact />
-          <Route path='/test' component={UserScreen} />
-          <Route path='/vlogin' component={VolunteerLoginScreen} />
-          <Route path='/volunteer' component={VolunteerScreen} />
-          <Route path='/question/:id' component={QuestionScreen} />
-          <Route path='/alogin' component={AdminLoginScreen} />
-          <Route path='/aregister' component={AdminRegisterScreen} />
-          <Route path='/admin/edit/:id' component={AdminEditQuestionScreen} />
-          <Route path='/admin' component={AdminScreen} />
+          <Switch>
+            <Route path='/test' component={UserScreen} />
+            <Route>
+              <Header />
+              <Switch>
+                <Route path='/' component={LoginScreen} exact />
+                <Route path='/vlogin' component={VolunteerLoginScreen} />
+                <Route path='/volunteer' component={VolunteerScreen} />
+                <Route path='/question/:id' component={QuestionScreen} />
+                <Route path='/alogin' component={AdminLoginScreen} />
+                <Route path='/aregister' component={AdminRegisterScreen} />
+                <Route
+                  path='/admin/edit/:id'
+                  component={AdminEditQuestionScreen}
+                />
+                <Route path='/admin' component={AdminScreen} />
+              </Switch>
+            </Route>
+          </Switch>
         </main>
       </ThemeProvider>
     </Router>
